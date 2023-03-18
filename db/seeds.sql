53de8f699c53638b028d625f7d822e012ea36797
Snowman271 <deanbenzmiller@yahoo.com>
2023-03-17 22:50:51 -0700

    Created outline and base code, with table creation

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (department_name)
+VALUES ("Engineering"),
+       ("Sales"),
+       ("Finance"), 
+       ("Legal");
+
+INSERT INTO role (title, salary)
+VALUES ("Sales Lead", 100000, 2),
+       ("Salesperson", 80000, 2),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 3),
+       ("lawyer", 125000, 4),
+       ("Wearhouse Guy", 500, 4);
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("John", "Doe"),
+       ("Mike", "Chan"),
+       ("Ashley", "Rodriguez"),
+       ("Kevin", "Tupik"),
+       ("Kunal", "Singh"),
+       ("Malia", "Brown"),
+       ("Bill", "Lourd");
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL Username
+    user: 'root',
+    // TODO: Add MySQL Password
+    password: 'coby200077',
+    database: 'employeeTracker_db'
+  },
+  console.log(`Connected to the employee tracker database.`)
+);
+
+app.listen(3001);
