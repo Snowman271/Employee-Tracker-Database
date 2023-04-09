ec9aa5cfd780cacd79e835afe9ca6a7e25ec27e2
Snowman271 <deanbenzmiller@yahoo.com>
2023-04-08 20:43:07 -0700

    updated database

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES ("Engineering"),
        ("Finance"), 
        ("Legal");
 
-INSERT INTO role (title, salary)
+INSERT INTO role (title, salary, department_id)
 VALUES ("Sales Lead", 100000, 2),
        ("Salesperson", 80000, 2),
        ("Lead Engineer", 150000, 1),
@@ -13,11 +13,12 @@ VALUES ("Sales Lead", 100000, 2),
        ("lawyer", 125000, 4),
        ("Wearhouse Guy", 500, 4);
 
-INSERT INTO employee (first_name, last_name)
-VALUES ("John", "Doe"),
-       ("Mike", "Chan"),
-       ("Ashley", "Rodriguez"),
-       ("Kevin", "Tupik"),
-       ("Kunal", "Singh"),
-       ("Malia", "Brown"),
-       ("Bill", "Lourd");
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("John", "Doe", 1, null),
+       ("Mike", "Chan", 2, 1),
+       ("Ashley", "Rodriguez", 2, 1),
+       ("Kevin", "Tupik", 2, 1),
+       ("Kunal", "Singh", 3, 1),
+       ("Malia", "Brown", 3, 1),
+       ("Bill", "Lourd", 4, 1);
+       
